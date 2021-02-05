a62a64a548a93c7aa9384181f2ddd065df61ab8d
JuyeolRyu <yujuyeol00@naver.com>
2021-02-05 10:25:43 +0900

    dp

--- 백수/코테공부/Dynamic_Programming/1로만들기.py
+++ 백수/코테공부/Dynamic_Programming/1로만들기.py
@@ -6,6 +6,9 @@
 #x를 1로 만든다.                                             #
 #############################################################
 
+#1로 만들기
+#https://www.acmicpc.net/problem/1463
+
 x = int(input())
 ans = 0
 dp = [0] * (x+1)
@@ -21,3 +24,18 @@ for i in range(2,x+1):
     dp[i] = min(dp[i],dp[i//2]+1)
 
 print(dp[x])
+
+
+###################################################################
+x = int(input())
+dp = [float('inf') for _ in range(x+1)]
+dp[-1] = 0
+
+for i in range(x,0,-1):
+  if i%3 == 0:
+    dp[i//3] = min(dp[i//3],1+dp[i])
+  if i%2 == 0:
+    dp[i//2] = min(dp[i//2],1+dp[i])
+  dp[i-1] = min(dp[i-1],1+dp[i])
+
+print(dp[1])
