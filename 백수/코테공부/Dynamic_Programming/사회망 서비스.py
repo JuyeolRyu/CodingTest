5a67289367ae89f50cca6a8c74bf17abe2dd9bce
JuyeolRyu <49298852+JuyeolRyu@users.noreply.github.com>
2021-04-23 11:52:39 +0900

    Update 사회망 서비스.py

--- 백수/코테공부/Dynamic_Programming/사회망 서비스.py
+++ 백수/코테공부/Dynamic_Programming/사회망 서비스.py
@@ -1,4 +1,35 @@
 #https://www.acmicpc.net/problem/2533
+#메모리 초과 해결 안됨
+
+#DFS & DP
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(500000)
+input = sys.stdin.readline
+
+sns = defaultdict(list)
+n = int(input())
+visited = []
+dp = [[1,0] for i in range(n+1)]
+
+for i in range(n-1):
+  u,v = map(int,input().split())
+  sns[u].append(v)
+  sns[v].append(u)
+
+def dfs(cur):
+  visited.append(cur)
+  for ne in sns[cur]:
+    if ne not in visited:
+      dfs(ne)
+      dp[cur][0] += dp[ne][1]
+      dp[cur][1] += dp[ne][0]
+
+dfs(1)
+print(min(dp[1]))
+
+#DFS 메모리 초과
+'''
 import sys
 from collections import defaultdict
 sys.setrecursionlimit(500000)
@@ -26,8 +57,9 @@ def dfs(cur):
 
 dfs(1)
 print(min(dp))
+'''
 
-
+#BFS 시간초과
 '''
 import sys
 from collections import defaultdict
