c2c3528c0015089fbf5264f1fdee9b0569f3d60a
JuyeolRyu <49298852+JuyeolRyu@users.noreply.github.com>
2021-08-09 16:03:41 +0900

    Add: 구현
    Python, Javascript

---
+++ 백수/algorithm/etc/부족한금액계산하기.py
@@ -0,0 +1,19 @@
+def solution(price, money, count):
+    answer = 0
+    for i in range(1,count+1):
+        answer += price*i
+    if answer < money:
+        return 0
+    return answer-money
+
+'''Javascript
+function solution(price, money, count) {
+    var answer = 0;
+    for(var i=1;i<=count;i++){
+        answer += price*i;
+    }
+    if(answer < money)
+        return 0;
+    return answer-money;
+}
+'''
