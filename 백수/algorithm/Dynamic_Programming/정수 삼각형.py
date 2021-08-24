bcbcc8d5f5cbd2e6faa8306b11536d63e823205b
JuyeolRyu <49298852+JuyeolRyu@users.noreply.github.com>
2021-08-24 12:24:08 +0900

    Update: dp

--- 백수/algorithm/Dynamic_Programming/정수 삼각형.py
+++ 백수/algorithm/Dynamic_Programming/정수 삼각형.py
@@ -11,3 +11,20 @@ for i in range(1,n):
     else:
       triangle[i][j] += max(triangle[i-1][j-1],triangle[i-1][j])
 print(max(triangle[-1]))
+
+#https://programmers.co.kr/learn/courses/30/lessons/43105
+def solution(t):
+    dp = []
+    for i in range(1,len(t)):
+        dp = [0]*len(t[i])
+        for j in range(len(t[i])):
+            if j == 0:
+                dp[j] = t[i][j]+t[i-1][j]
+            elif j == len(t[i])-1:
+                dp[j] = t[i][j]+t[i-1][j-1]
+            else:
+                dp[j] = max(dp[j], t[i][j]+t[i-1][j-1], t[i][j]+t[i-1][j])
+                
+        t[i] = dp
+
+    return max(t[-1])
